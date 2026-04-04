81e4011c7ad1411ad28e8cd90697535a86e3b158
bestleey <yurekazzz@gmail.com>
2026-04-04 22:30:07 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="leave-tracker-viewer-v4";
+const CACHE_NAME="leave-tracker-viewer-v5";
 const FILES=["./","./index.html","./config.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-180.png"];
 self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE_NAME).then((c)=>c.addAll(FILES)))});
 self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((k)=>k!==CACHE_NAME).map((k)=>caches.delete(k)))).then(()=>self.clients.claim()))});
